66957d4cc70fa001639778c25ecfe918a59c5893
Nisarg <nisargs2001@gmail.com>
2023-02-17 18:45:33 +0530

    build updated

--- app.js
+++ app.js
@@ -11,4 +11,15 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/", infoRouter);
 
+// serving the frontend
+app.use(express.static(path.join(__dirname, "./frontend/build")));
+app.get("*", function (_, res) {
+  res.sendFile(
+    path.join(__dirname, "./frontend/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
+});
+
 module.exports = app;
